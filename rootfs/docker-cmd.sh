26d021fd57e1761caa8d6d6b5597b5acbea68767
Socheat Sok <socheatsok78@gmail.com>
2023-07-06 10:45:41 +0700

    Cleanup rootfs/docker-cmd.sh

--- rootfs/docker-cmd.sh
+++ rootfs/docker-cmd.sh
@@ -3,14 +3,12 @@ set -e
 . /etc/os-release
 
 ME=$(basename "$0")
-PID=$$
 logv() { echo "$ME [$(date)]: $* "; }
 
 LIBREOFFICE_VERSION=$(libreoffice --version)
 PYTHON_VERSION=$(python --version)
 JRE_VERSION=$(java --version)
 
-logv "Main process running pid $PID"
 logv "$NAME version: $VERSION_ID"
 logv "Python version: $PYTHON_VERSION"
 echo "$JRE_VERSION" | while read -r a; do logv "$a"; done
