71456f2c5f8e9468cd9f3b98c3e3543bfbbc27e3
Socheat Sok <alex@socheat.net>
2023-07-23 03:30:11 +0700

    Rework unoserver-rest-api service and dependencies

--- rootfs/docker-cmd.sh
+++ rootfs/docker-cmd.sh
@@ -1,18 +1,3 @@
 #!/bin/sh
 set -e
-. /etc/os-release
-
-ME=$(basename "$0")
-logv() { echo "$ME [$(date)]: $* "; }
-
-LIBREOFFICE_VERSION=$(libreoffice --version)
-PYTHON_VERSION=$(python --version)
-JRE_VERSION=$(java --version)
-
-logv "$NAME version: $VERSION_ID"
-logv "Python version: $PYTHON_VERSION"
-echo "$JRE_VERSION" | while read -r a; do logv "$a"; done
-logv "$LIBREOFFICE_VERSION"
-
 while true; do sleep 1; done
-# sleep infinity
