1d6dad0a740a04ec908a5e7295f254d4a5ba9b9a
Socheat Sok <alex@socheat.net>
2024-04-17 21:49:35 +0700

    Rework buildfs/install-unoserver.sh

--- buildfs/install-unoserver.sh
+++ buildfs/install-unoserver.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
+BASHRC_FILE="/etc/bash/bashrc"
+
+# Fallback to root's .bashrc if /etc/bash/bashrc does not exist
+if [ ! -f "${BASHRC_FILE}" ]; then
+	BASHRC_FILE="/root/.bashrc"
+fi
 
 # Activate Python virtual environment
-# Install unoserver
-# Prepare /etc/bash/bashrc
 PYTHON_VIRTUALENV_PATH="${PYTHON_VIRTUALENV_PATH:-/venv}"
-python3 -m venv "${PYTHON_VIRTUALENV_PATH}" && {
-	source "${PYTHON_VIRTUALENV_PATH}/bin/activate"
-	pip3 install --no-cache unoserver==1.6
-	echo 'PYTHON_VIRTUALENV_PATH="${PYTHON_VIRTUALENV_PATH:-/venv}"' >> /etc/bash/bashrc
-	echo 'source "${PYTHON_VIRTUALENV_PATH}/bin/activate"' >> /etc/bash/bashrc
-}
+python3 -m venv "${PYTHON_VIRTUALENV_PATH}"
+
+# Install unoserver
+source "${PYTHON_VIRTUALENV_PATH}/bin/activate"
+pip3 install --no-cache unoserver==1.6
+
+# Prepare BASHRC_FILE
+echo 'PYTHON_VIRTUALENV_PATH="${PYTHON_VIRTUALENV_PATH:-/venv}"' >> ${BASHRC_FILE}
+echo 'source "${PYTHON_VIRTUALENV_PATH}/bin/activate"' >> ${BASHRC_FILE}
